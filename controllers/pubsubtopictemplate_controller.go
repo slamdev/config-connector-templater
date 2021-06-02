6d943e363af6e84e9937b4c8a30cb139f4ab0792
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2021-06-02 21:22:13 +0200

    create templated resource

--- controllers/pubsubtopictemplate_controller.go
+++ controllers/pubsubtopictemplate_controller.go
@@ -18,17 +18,21 @@ package controllers
 
 import (
 	"context"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/pubsub/v1beta1"
-	v1 "k8s.io/api/core/v1"
+	"fmt"
+	pubsub "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/pubsub/v1beta1"
+	"github.com/Masterminds/sprig/v3"
+	api "github.com/slamdev/config-connector-templater/api/v1alpha1"
+	"html/template"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	configconnectortemplaterv1alpha1 "github.com/slamdev/config-connector-templater/api/v1alpha1"
+	"strings"
 )
 
 // PubSubTopicTemplateReconciler reconciles a PubSubTopicTemplate object
@@ -48,7 +52,7 @@ func (r *PubSubTopicTemplateReconciler) Reconcile(ctx context.Context, req ctrl.
 	logger := log.FromContext(ctx).WithValues("pubsubtopictemplate", req.NamespacedName)
 
 	// Fetch the Memcached instance
-	templateRes := &configconnectortemplaterv1alpha1.PubSubTopicTemplate{}
+	templateRes := &api.PubSubTopicTemplate{}
 	err := r.Get(ctx, req.NamespacedName, templateRes)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -61,11 +65,15 @@ func (r *PubSubTopicTemplateReconciler) Reconcile(ctx context.Context, req ctrl.
 	}
 
 	// Check if the PubSubTopic already exists, if not create a new one
-	found := &v1beta1.PubSubTopic{}
+	found := &pubsub.PubSubTopic{}
 	err = r.Get(ctx, types.NamespacedName{Name: templateRes.Name, Namespace: templateRes.Namespace}, found)
 	if err != nil && errors.IsNotFound(err) {
 		// Define a new PubSubTopic resource
-		res := r.createTemplatedResource(templateRes)
+		res, err := r.createTemplatedResource(templateRes)
+		if err != nil {
+			logger.Error(err, "Failed to create templated resource")
+			return ctrl.Result{}, err
+		}
 		logger.Info("Creating a new PubSubTopic", "PubSubTopic.Namespace", res.Namespace, "PubSubTopic.Name", res.Name)
 		err = r.Create(ctx, res)
 		if err != nil {
@@ -80,7 +88,12 @@ func (r *PubSubTopicTemplateReconciler) Reconcile(ctx context.Context, req ctrl.
 	}
 
 	// Build the PubSubTopic spec from PubSubTopicTemplate
-	resSpec := r.createTemplatedResource(templateRes).Spec
+	res, err := r.createTemplatedResource(templateRes)
+	if err != nil {
+		logger.Error(err, "Failed to create templated resource")
+		return ctrl.Result{}, err
+	}
+	resSpec := res.Spec
 
 	// Update PubSubTopic if needed
 	if !reflect.DeepEqual(resSpec, found.Spec) {
@@ -93,7 +106,7 @@ func (r *PubSubTopicTemplateReconciler) Reconcile(ctx context.Context, req ctrl.
 	}
 
 	// Build the PubSubTopicTemplate status ref with from PubSubTopic
-	ref := v1.ObjectReference{
+	ref := corev1.ObjectReference{
 		Kind:            found.Kind,
 		Namespace:       found.Namespace,
 		Name:            found.Name,
@@ -115,14 +128,62 @@ func (r *PubSubTopicTemplateReconciler) Reconcile(ctx context.Context, req ctrl.
 	return ctrl.Result{}, nil
 }
 
-func (r *PubSubTopicTemplateReconciler) createTemplatedResource(template *configconnectortemplaterv1alpha1.PubSubTopicTemplate) *v1beta1.PubSubTopic {
-	return &v1beta1.PubSubTopic{}
+func (r *PubSubTopicTemplateReconciler) createTemplatedResource(template *api.PubSubTopicTemplate) (*pubsub.PubSubTopic, error) {
+	spec, err := r.render(template.Spec, template)
+	if err != nil {
+		return nil, fmt.Errorf("failed to render template; %w", err)
+	}
+	res := &pubsub.PubSubTopic{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        template.Name,
+			Namespace:   template.Namespace,
+			Labels:      template.Labels,
+			Annotations: template.Annotations,
+		},
+		Spec: spec.(pubsub.PubSubTopicSpec),
+	}
+	if err := ctrl.SetControllerReference(template, res, r.Scheme); err != nil {
+		return nil, fmt.Errorf("failed to set ctrl ref; %w", err)
+	}
+	return res, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *PubSubTopicTemplateReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&configconnectortemplaterv1alpha1.PubSubTopicTemplate{}).
-		Owns(&v1beta1.PubSubTopic{}).
+		For(&api.PubSubTopicTemplate{}).
+		Owns(&pubsub.PubSubTopic{}).
 		Complete(r)
+}
+
+func (r *PubSubTopicTemplateReconciler) render(spec interface{}, data interface{}) (interface{}, error) {
+	if err := r.renderValue(reflect.ValueOf(spec).Elem(), data); err != nil {
+		return nil, fmt.Errorf("failed to render spec; %w", err)
+	}
+	return spec, nil
+}
+
+func (r *PubSubTopicTemplateReconciler) renderValue(v reflect.Value, data interface{}) error {
+	s := v
+	tpl := template.New("_").Funcs(sprig.FuncMap())
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		if f.Kind() == reflect.String && f.CanSet() {
+			parsed, err := tpl.Parse(f.String())
+			if err != nil {
+				return fmt.Errorf("failed to parse template; %w", err)
+			}
+			v := new(strings.Builder)
+			if err := parsed.Execute(v, data); err != nil {
+				return fmt.Errorf("failed to execute template; %w", err)
+			}
+			f.SetString(v.String())
+		} else if f.Kind() == reflect.Struct {
+			x1 := reflect.ValueOf(f.Interface())
+			if err := r.renderValue(x1, data); err != nil {
+				return fmt.Errorf("failed to render value; %w", err)
+			}
+		}
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/GoogleCloudPlatform/k8s-config-connector v1.51.1
+	github.com/Masterminds/sprig/v3 v3.2.2
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.2
 	k8s.io/api v0.20.2
